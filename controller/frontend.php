ce3be3cc119924557536801171737dbc61c1725f
kjunpark92 <45029422+kjunpark92@users.noreply.github.com>
2019-03-11 15:24:51 +0900

    divide sections

---
+++ controller/frontend.php
@@ -0,0 +1,12 @@
+<?php 
+require_once('model/ManagerDB.php');
+
+function viewHome()
+{
+    require('view/frontend/viewHome.php');
+}
+
+function viewCalendar()
+{
+    require('view/frontend/viewCalendar.php');
+}
