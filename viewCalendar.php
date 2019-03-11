d1303582b7a3426f10e8bb4c436749536ef63b7d
kjunpark92 <45029422+kjunpark92@users.noreply.github.com>
2019-03-11 15:21:43 +0900

    Delete viewCalendar.php

--- viewCalendar.php
+++
@@ -1,8 +0,0 @@
-<?php $title = 'Weekly Scheduler'; ?>
-<?php ob_start(); ?>
-<?php require_once "navbar.php"?>
-<?php require_once "calendar.php"?>
-<?php require_once "footer.php"?>
-<?php
-$content = ob_get_clean();
-require('../template.php'); ?>
