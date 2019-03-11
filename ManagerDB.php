e57cbef65f97612a1c08badde26a2cf4a7804549
kjunpark92 <45029422+kjunpark92@users.noreply.github.com>
2019-03-11 15:21:54 +0900

    Delete ManagerDB.php

--- ManagerDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ManagerDB {
-    protected function dbConnect(){
-        $bdd = new PDO('mysql:host=localhost;dbname=weekly_scheduler;charset=utf8', 'root', 'root');
-        return $bdd;
-    }
-}
