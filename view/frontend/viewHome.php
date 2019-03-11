471c6f44a5c349b74eb561197f046877316e5981
kjunpark92 <45029422+kjunpark92@users.noreply.github.com>
2019-03-11 15:36:36 +0900

    divide sections

---
+++ view/frontend/viewHome.php
@@ -0,0 +1,9 @@
+<?php $title = 'Weekly Scheduler'; ?>
+<?php ob_start(); ?>
+<?php require_once "navbar.php"?>
+<?php require_once "carousel.php"?>
+<?php require_once "login.php"?>
+<?php require_once "footer.php"?>
+<?php
+$content = ob_get_clean();
+require('../template.php'); ?>
