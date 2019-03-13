1aebd2e4806a2e67f07106cced17fa0d45b7fa03
hlim18 <j.lim.j101@gmail.com>
2019-03-13 15:55:03 +0900

    updated a relative path in viewHome.php

--- view/frontend/viewHome.php
+++ view/frontend/viewHome.php
@@ -4,6 +4,5 @@
 <?php require_once "carousel.php"?>
 <?php require_once "login.php"?>
 <?php require_once "footer.php"?>
-<?php
-$content = ob_get_clean();
-require('../template.php'); ?>
+<?php $content = ob_get_clean();?>
+<?php require "./view/template.php"; ?>
