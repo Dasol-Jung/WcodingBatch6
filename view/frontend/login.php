4042a700c59edc17bcf0f9a328a1e98829b2d138
Dasol Jung <xenosophian@gmail.com>
2019-03-13 17:19:32 +0900

    login.php html and css

--- view/frontend/login.php
+++ view/frontend/login.php
@@ -1,3 +1,21 @@
-<section>
-login
-</section>
+<link rel="stylesheet" href="../../public/css/login.css"/>
+<form class="loginForm" action="index.php" method="POST">
+
+    <label for="email">Email</label>
+    <input name="email" id="email" type="text">
+    <label for="password">Password</label>
+    <input name="password" id="password" type="password">
+    <label for="confirmPassword">Confirm Password</label>
+    <input name="confirmPassword" id="confirmPassword" type="password">
+
+    <button id="signupBtn">Sign Up</button>
+    <div class="socialLoginContainer">
+        <button class="googleSignup"><img id="googleLogo" src="../../public/images/googleLogo.png"/><span>Google</span></button>
+        <button class="kakaoSignup"><img id="kakaoLogo" src="../../public/images/kakaoLogo.png"/><span>Sign Up</span></button>
+    </div>
+    
+    <div class="toSigninContainer">
+        <span class="toSignin">Already have an account? <a href="index.php/signin">Sign in</a></span>
+    </div>
+
+</form>
