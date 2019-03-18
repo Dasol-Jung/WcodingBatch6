cdd4e6922f1cde0c8c6af7856cd2668731134e6b
Dasol Jung <xenosophian@gmail.com>
2019-03-18 21:42:32 +0900

    1. edit header 2. add schedule button

--- view/frontend/header.php
+++ view/frontend/header.php
@@ -13,6 +13,8 @@
         <ul class="menuWrapper">
             <?php if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']==true):?>
                 <li class="menuItem"><a class='menuLink' href="index.php">HOME</a></li>
+                <li class="menuItem"><a class='menuLink' href="index.php">AVATAR</a></li>
+                <li class="menuItem"><a class='menuLink' href="index.php">PROFILE</a></li>
                 <li class="menuItem"><a class='menuLink' href="index.php?action=weeklySchedule">SCHEDULER</a></li>
                 <li class="menuItem"><a class='menuLink' href="index.php?action=logout">LOGOUT</a></li>
             <?php else :?>
