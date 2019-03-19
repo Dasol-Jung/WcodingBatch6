78fa714f599ae9f1310198d011c95330f1692f86
Jun <xjun852x@hotmail.com>
2019-03-19 17:04:51 +0900

    added weekly/monthly

--- public/js/frontend/viewWeekly.js
+++ public/js/frontend/viewWeekly.js
@@ -3,3 +3,9 @@
 (function createAddButtonEvent() {
 	document.querySelector('button.addSchedule').addEventListener('click', () => {});
 })();
+
+(function changeWeeklyMonthly(){
+	document.querySelector('button.weeklyMonthly').addEventListener("click",() => {
+			window.location.href = 'http://localhost:8888/index.php?action=monthlySchedule'
+	})
+})();
